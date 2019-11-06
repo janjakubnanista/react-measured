44b62134f96896380ecc3e438738a46302ac3fe5
Jan Jakub Nanista <jan.jakub.nanista@gmail.com>
2019-11-06 10:23:58 +0100

    Clean types.ts in core

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -11,50 +11,18 @@ export interface BoundingBox {
 
 export type BoundingBoxProviderChildrenFunction = (props: BoundingBox) => React.ReactNode;
 
-export interface BoundingBoxProviderOwnProps {
-  boundingBoxTransforms?: CheckerTransformInput[];
-  children?: BoundingBoxProviderChildrenFunction | React.ReactNode;
-  onBoundingBoxChange?: (props: BoundingBox | undefined) => void;
-  positionOnly?: boolean;
-  sizeOnly?: boolean;
-}
-
-export type BoundingBoxProviderWrappedComponentProps = React.PropsWithChildren<{}>;
-
-export type BoundingBoxProviderProps<P extends React.PropsWithChildren<{}>> = BoundingBoxProviderOwnProps &
-  Omit<P, keyof BoundingBoxProviderOwnProps>;
-
 // Checker types
 export type CheckerRef<T> = React.RefObject<T | undefined | null>;
 
 export type CheckerTransform<T> = (value: T) => BoundingBox;
 
-export type CheckerUnregisterCallback = () => void;
-
 export type CheckerChangeHandler = (boundingBox: BoundingBox | undefined) => void;
 
-export interface CheckerItem<T> {
-  ref: CheckerRef<T>;
-  transforms: CheckerTransform<BoundingBox>[];
-  onChange: CheckerChangeHandler;
-}
-
-export interface CheckerImplementation<T> {
-  register: (
-    ref: CheckerRef<T>,
-    onChange: CheckerChangeHandler,
-    ...transforms: CheckerTransformInput[]
-  ) => CheckerUnregisterCallback;
+export interface Checker<T> {
+  register: (ref: CheckerRef<T>, onChange: CheckerChangeHandler, ...transforms: CheckerTransformInput[]) => () => void;
   start: () => void;
   stop: () => void;
   clear: () => void;
 }
 
 export type CheckerTransformInput = CheckerTransform<BoundingBox> | null | false | undefined;
-
-// Hook types
-export type UseBoundingBox<T> = (
-  ref: CheckerRef<T>,
-  onChange?: CheckerChangeHandler,
-  ...transforms: CheckerTransformInput[]
-) => BoundingBox | undefined;
