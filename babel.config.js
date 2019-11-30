ed7b8a248470afcc333f36ff1678c57d44fd082b
Jan Jakub Nanista <jan.jakub.nanista@gmail.com>
2019-11-30 19:14:33 +0100

    Use babel based build

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  presets: ['@babel/env', '@babel/preset-react', '@babel/typescript'],
+  plugins: ['@babel/proposal-class-properties', '@babel/proposal-object-rest-spread'],
+  sourceType: 'unambiguous',
+};
